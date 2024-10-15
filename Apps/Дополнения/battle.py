5bc089a54078b9957112e57092a0da108576c610
Тарасов Андрей <vb2005@yandex.ru>
2024-10-15 22:33:21 +0300

    Добавил файл для соревнований

---
+++ Apps/Дополнения/battle.py
@@ -0,0 +1,16 @@
+import requests
+import json
+def sendResults(user, groupId, taskId, model, history):
+  loss = model.loss
+  epochs = max(history.epoch)
+  trainloss = min(history.history['loss'])
+  valloss = min(history.history['val_loss'])
+  weights = model.count_params()
+
+  url = 'https://dnn.vb2005.ru/api/SendResults'
+  modelInfo = {"validLoss" : valloss, "trainLoss" : trainloss, "lossFunc": loss, "epochs" : epochs, "weights" : weights}
+  userResult = {"username" : user, "groupId" : groupId, "taskId" : taskId, "modelInfo" : modelInfo}
+  x = requests.post(url, json = userResult)
+  if (x.status_code == 200):
+    print('Результат записан')
+
